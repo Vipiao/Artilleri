dd414b35c7dcad716f4314df5358658a7df4ac2f
Stian Finjord <stian.finjord@gmail.com>
2017-06-20 20:34:26 +0200

    init

--- Main.js
+++ Main.js
@@ -1,14 +1,21 @@
 
 var fps;
 var loop;
-
-
+var grav;
+var balls;
+var ball1;
+var ball2;
 
 
 window.onload = init;
+
 function init(){
 	fps = 60;
 	loop = setInterval(fps / 1000);
+	grav = new Vec(0.1);
+	ball1 = new Ball(new Vec(100, 100), new Vec(3, -4), 10, 1, "blue");
+	ball2 = new Ball(new Vec(100, 100), new Vec(0.5, -3), 10, 2, "black");
+	balls = [ball1, ball2];
 }
 function gameLoop(){
 	
