c4e8c67fcf351afe4c97c13d5042020f8a767f19
Vipiao <markus@fjellheim.org>
2017-06-20 19:14:48 +0200

    Add and sub added to Vec

--- Vec.js
+++ Vec.js
@@ -9,7 +9,14 @@ Vec.add(v1, v2){
 Vec.sub(v1, v2){
 	return new Vec(v1.x - v2.x, v1.y - v2.y);
 }
-
+Vec.prototype.add(v){
+	this.x += v.x;
+	this.y += v.y;
+}
+Vec.prototype.sub(v){
+	this.x -= v.x;
+	this.y -= v.y;
+}
 
 
 
