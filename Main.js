55818d8c0436ee609861752cd0ea9f3d87fe404a
Stian Finjord <stian.finjord@gmail.com>
2017-06-20 21:26:20 +0200

    More on main

--- Main.js
+++ Main.js
@@ -12,24 +12,26 @@ window.onload = init;
 function init(){
 	fps = 60;
 	loop = setInterval(gameLoop, fps / 1000);
-	grav = new Vec(0.1);
+	grav = new Vec(0, -0.01);
 	draw = new Draw("canvas");
 	
-	ball1 = new Ball(new Vec(100, 100), new Vec(0.3, -0.4), 10, 1, "blue");
-	ball2 = new Ball(new Vec(100, 100), new Vec(0.05, -0.3), 10, 2, "black");
+	ball1 = new Ball(new Vec(100, 100), new Vec(0.3, 2), 2, 1, "blue");
+	ball2 = new Ball(new Vec(100, 100), new Vec(0.05, 1.3), 2, 2, "black");
 	balls = [ball1, ball2];
 }
 function gameLoop(){
 	draw.clear();
 	
 	//do physics
-	balls[0].pos.add(balls[0].vel);
-	balls[1].pos.add(balls[1].vel);
-	
-	
+	for(var i = 0; i < balls.length; i++) {
+		balls[i].pos.add(balls[i].vel.add(grav));
+	}
+
 	//draw balls
-	balls[0].draw();
-	balls[1].draw();
+	for(var i = 0; i < balls.length; i++) {
+		balls[i].draw();
+	}
+	
 }
 
 
